6510347d4debbc82aceec6da3441db52a794a60b
Soufiane Ait Elghazi <soufianeaitelghazi@gmail.com>
2022-04-17 10:46:51 -0700

    Initial Commit

---
+++ CULTUROC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CULTUROC
+//
+//  Created by Soufiane AIT ELGHZI on 4/17/22.
+//  Copyright © 2022 Soufiane AIT ELGHZI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
